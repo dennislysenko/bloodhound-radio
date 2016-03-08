0d04b95b2b222a6e6e203028c15b088145630c29
Dennis Lysenko <dennis.s.lysenko@gmail.com>
2016-03-08 02:36:09 -0500

    some cleanup

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -3,14 +3,5 @@ class WelcomeController < ApplicationController
     @current_user = current_user
     @logged_in = logged_in?
     @user_id = session[:user_id]
-
-    if logged_in?
-      # @tracks = Rails.cache.fetch("sc/user/likes/#{current_user.id}", expires_in: 1.day) do
-      #   client = EasySoundcloud.client_for(current_user)
-      #   client.get('/me/favorites').as_json
-      # end
-      #
-      # @scents = Scent.where(user: current_user).map { |scent| ScentSerializer.new(scent).as_json }
-    end
   end
 end
