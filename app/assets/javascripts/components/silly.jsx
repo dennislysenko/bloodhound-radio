84233e3d0a20900122c59465a0d684ff3a51d2aa
Dennis Lysenko <dennis.s.lysenko@gmail.com>
2016-03-07 19:19:50 -0500

    add first react component

---
+++ app/assets/javascripts/components/silly.jsx
@@ -0,0 +1,14 @@
+/**
+ * Created by dennis on 3/7/16.
+ */
+var Silly = React.createClass({
+    getInitialState() {
+        return {};
+    },
+
+    render() {
+        return (
+            <div>Silly {this.props.thing}!</div>
+        );
+    }
+});
